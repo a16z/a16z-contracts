4a2a23c3f5d3edd1529ef11afdd600ae42e3ca29
fmhall <masonhall@gmail.com>
2022-05-25 12:12:33 -0400

    clean up import

--- contracts/examples/MyToken.sol
+++ contracts/examples/MyToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "../licenses/CantBeEvil.sol";
+import {LicenseVersion, CantBeEvil} from "../licenses/CantBeEvil.sol";
 
 contract MyToken is ERC721, Ownable, CantBeEvil {
     constructor() ERC721("MyToken", "MTK") CantBeEvil(LicenseVersion.CR) {}
