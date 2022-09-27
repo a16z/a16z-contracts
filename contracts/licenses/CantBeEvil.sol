f3335c0077fbca7195d68f0e5413b0b40a7515ad
Stevewu1234 <plgawu@163.com>
2022-09-27 15:54:58 +0800

    update the License library contract version separate LicenseVersion to an Library for integrating easily with contract

--- contracts/licenses/CantBeEvil.sol
+++ contracts/licenses/CantBeEvil.sol
@@ -5,45 +5,46 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "./ICantBeEvil.sol";
-
-enum LicenseVersion {
-    CBE_CC0,
-    CBE_ECR,
-    CBE_NECR,
-    CBE_NECR_HS,
-    CBE_PR,
-    CBE_PR_HS
-}
+import "./License.sol";
 
 contract CantBeEvil is ERC165, ICantBeEvil {
-    using Strings for uint;
-    string internal constant _BASE_LICENSE_URI = "ar://_D9kN1WrNWbCq55BSAGRbTB4bS3v8QAPTYmBThSbX3A/";
-    LicenseVersion public licenseVersion; // return string
-    constructor(LicenseVersion _licenseVersion) {
-        licenseVersion = _licenseVersion;
-    }
+  using Strings for uint256;
+  string internal constant _BASE_LICENSE_URI = "ar://_D9kN1WrNWbCq55BSAGRbTB4bS3v8QAPTYmBThSbX3A/";
+  License.LicenseVersion public licenseVersion; // return string
 
-    function getLicenseURI() public view returns (string memory) {
-        return string.concat(_BASE_LICENSE_URI, uint(licenseVersion).toString());
-    }
+  constructor(License.LicenseVersion _licenseVersion) {
+    licenseVersion = _licenseVersion;
+  }
 
-    function getLicenseName() public view returns (string memory) {
-        return _getLicenseVersionKeyByValue(licenseVersion);
-    }
+  function getLicenseURI() public view returns (string memory) {
+    return string.concat(_BASE_LICENSE_URI, uint256(licenseVersion).toString());
+  }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165) returns (bool) {
-        return
-            interfaceId == type(ICantBeEvil).interfaceId ||
-            super.supportsInterface(interfaceId);
-    }
+  function getLicenseName() public view returns (string memory) {
+    return _getLicenseVersionKeyByValue(licenseVersion);
+  }
 
-    function _getLicenseVersionKeyByValue(LicenseVersion _licenseVersion) internal pure returns (string memory) {
-        require(uint8(_licenseVersion) <= 6);
-        if (LicenseVersion.CBE_CC0 == _licenseVersion) return "CBE_CC0";
-        if (LicenseVersion.CBE_ECR == _licenseVersion) return "CBE_ECR";
-        if (LicenseVersion.CBE_NECR == _licenseVersion) return "CBE_NECR";
-        if (LicenseVersion.CBE_NECR_HS == _licenseVersion) return "CBE_NECR_HS";
-        if (LicenseVersion.CBE_PR == _licenseVersion) return "CBE_PR";
-        else return "CBE_PR_HS";
-    }
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    virtual
+    override(ERC165)
+    returns (bool)
+  {
+    return interfaceId == type(ICantBeEvil).interfaceId || super.supportsInterface(interfaceId);
+  }
+
+  function _getLicenseVersionKeyByValue(License.LicenseVersion _licenseVersion)
+    internal
+    pure
+    returns (string memory)
+  {
+    require(uint8(_licenseVersion) <= 6);
+    if (License.LicenseVersion.CBE_CC0 == _licenseVersion) return "CBE_CC0";
+    if (License.LicenseVersion.CBE_ECR == _licenseVersion) return "CBE_ECR";
+    if (License.LicenseVersion.CBE_NECR == _licenseVersion) return "CBE_NECR";
+    if (License.LicenseVersion.CBE_NECR_HS == _licenseVersion) return "CBE_NECR_HS";
+    if (License.LicenseVersion.CBE_PR == _licenseVersion) return "CBE_PR";
+    else return "CBE_PR_HS";
+  }
 }
---
+++ contracts/licenses/License.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+library License {
+  enum LicenseVersion {
+    CBE_CC0,
+    CBE_ECR,
+    CBE_NECR,
+    CBE_NECR_HS,
+    CBE_PR,
+    CBE_PR_HS
+  }
+}
